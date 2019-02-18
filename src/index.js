b26060efd29e7ee27330a54ce633d1dc07d69ca1
abishek.g <abishek.g@mail.aquilogix.com>
2019-02-18 09:36:33 +0530

    Season Modifications

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import { Spinner } from './Spinner';
 
-const App = () => {
-  return (
-    <div>
-      Hi There!
-    </div>
-  )
+class App extends Component {
+
+  state = {
+    lat: '',
+    errorMessage: ''
+  }
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (error) => this.setState({ errorMessage: error.message })
+    )
+  }
+
+
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error:{this.state.errorMessage}</div>
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div><SeasonDisplay lat={this.state.lat} /></div>
+    }
+    return (
+      <Spinner message="Please accept location request" />
+    )
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
