f3c271040592e00d11dd819d0df7259ace80f778
abishek.g <abishek.g@mail.aquilogix.com>
2019-02-27 10:27:08 +0530

    Hooks Implemented

--- src/index.js
+++ src/index.js
@@ -1,33 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component, useState, useEffect } from 'react'
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import { Spinner } from './Spinner';
+import useLocation from './useLocation';
 
-class App extends Component {
+const App = () => {
 
-  state = {
-    lat: '',
-    errorMessage: ''
+  const [lat, errorMessage] = useLocation();
+
+  let content;
+  if (errorMessage) {
+    content = <div>Error:{errorMessage}</div>
+  } else if (lat) {
+    content = <SeasonDisplay lat={lat} />;
+  } else {
+    content = <Spinner message="Please accept location request" />
   }
 
-  componentDidMount() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => this.setState({ lat: position.coords.latitude }),
-      (error) => this.setState({ errorMessage: error.message })
-    )
-  }
-
-  render() {
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error:{this.state.errorMessage}</div>
-    }
-    if (!this.state.errorMessage && this.state.lat) {
-      return <div><SeasonDisplay lat={this.state.lat} /></div>
-    }
-    return (
-      <Spinner message="Please accept location request" />
-    )
-  }
+  return <div className="border red">{content}</div>
 }
+
+
 
 ReactDOM.render(<App />, document.querySelector('#root'));
